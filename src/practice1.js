597b1fc328d36e4adc70f298c4477dbd2f75e3bf
ClarkTheSup <lknkoqq123@163.com>
2020-09-11 14:53:06 +0800

    [kangning] feat: pass practice1 test

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,11 @@
-const parseData
+const parseData = (input) => {
+    const {data} = input;
+    const output = [];
+    data.map((obj) => {
+        const [name, age, gender, birthday] = obj;
+        const newObj = { name, age, gender, birthday };
+        output.push(newObj)
+    })
+    return output;
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,9 @@
-const inject
+const inject = (items, sections) => {
+    sections = sections.sort((obj1, obj2) => obj1.index < obj2.index ? 1 : -1);
+    sections.map((obj) => {
+        const {content, index} = obj;
+        items.splice(index, 0, content);
+    });
+    return items;
+}
 export { inject };
