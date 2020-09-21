c871c04d10c0032ef3bb2f627f31983650c2c3d7
kevindongzg <dongkevin94@gmail.com>
2020-09-21 23:47:42 +0800

    add TODO

--- src/practice1.js
+++ src/practice1.js
@@ -1,7 +1,10 @@
+// TODO 没有使用column
 const parseData = (input) => {
     const {data} = input;
+    // TODO 使用map做数组与数组的转化，不用声明output，查查map的使用
     const output = [];
     data.map((obj) => {
+        // TODO 不要hardcode这四个key，这是后端返回的，可能会发生变化
         const [name, age, gender, birthday] = obj;
         const newObj = { name, age, gender, birthday };
         output.push(newObj)
--- src/practice2.js
+++ src/practice2.js
@@ -1,5 +1,7 @@
 const inject = (items, sections) => {
+    // TODO 这里 obj1.index， obj2.index直接相减就好
     sections = sections.sort((obj1, obj2) => obj1.index < obj2.index ? 1 : -1);
+    // TODO 不是map的使用场景
     sections.map((obj) => {
         const {content, index} = obj;
         items.splice(index, 0, content);
